9e6155c2f8f7e1f813bc8b5a034e0f2c1cc50a51
IpsumDominum <30939978+IpsumDominum@users.noreply.github.com>
2020-02-28 13:13:55 +1300

    shalom

---
+++ main.py
@@ -0,0 +1,29 @@
+"""Shalom, Template generator for the web
+
+Uses Jinja
+
+Overlay Structure:
+[
+    parse a structure description language
+]
+->
+[
+    Project Abstraction Graph
+]
+->
+[
+    
+]
+Template Definition ->
+[
+    
+]
+
+
+
+
+
+"""
+
+
+
